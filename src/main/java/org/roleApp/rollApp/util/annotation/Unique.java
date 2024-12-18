9ac9706e82ac92998707572d953d1ed9b48c989b
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-18 11:09:53 +0100

    add util package, with helping utilities like enums, validation logic, and annotations

---
+++ src/main/java/org/roleApp/rollApp/util/annotation/Unique.java
@@ -0,0 +1,19 @@
+package org.roleApp.rollApp.util.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.roleApp.rollApp.util.validation.UniqueConstraintValidator;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueConstraintValidator.class)
+@Documented
+public @interface Unique {
+    String message() default "Value must be unique";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<?> entityClass();
+    String field();
+}
